880410c1564182dfd73c180d36911657b622c554
DevShara <developer.ishara@gmail.com>
2023-01-16 08:22:33 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+const App = () => {
+  return (
+    <>
+      <h1>RENDERED</h1>
+    </>
+  );
+};
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(React.createElement(App));
